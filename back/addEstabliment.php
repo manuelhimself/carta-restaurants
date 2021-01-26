eab376b75756de8da0f552cf6c22e8a1d906fc71
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-01-26 19:23:50 +0100

    registre restaurants

---
+++ back/addEstabliment.php
@@ -0,0 +1,42 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "restaurants");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+if(isset($_POST['establiment'], $_POST['email'], $_POST['numComensals'],$_POST['tel'],$_POST['poblacio'],$_POST['paswd'])){
+
+    $e = $_POST['establiment'];
+    $email = $_POST['email'];
+    $numCom = $_POST['numComensals'];
+    $tlfn = $_POST['tel'];
+    $poblacio = $_POST['poblacio'];
+    $passwd = $_POST['paswd'];
+    
+}
+
+$sql = " INSERT INTO establiment (nom,correu_electronic,num_comensals,telefon,Poblacio_id,password) VALUES ('$e','$email','$numCom','$tlfn','$poblacio','$passwd')";
+
+if($conn->query($sql) === TRUE){
+
+  $last_id = $conn->insert_id;
+ 
+  $categoria=$_POST["id"];
+  $count = count($categoria);
+  for ($i = 0; $i < $count; $i++) {
+      $idCateg = $categoria[$i];
+      $cat = " INSERT INTO categoria_establiment (Establiment_id,Categoria_id) VALUES ('$last_id','$idCateg')";
+
+      if ($conn->query($cat) === TRUE) {
+        echo "New record created successfully";
+      } else {
+        echo "Error: " . $cat . "<br>" . $conn->error;
+      }
+
+  }
+} else {
+  printf("Errormessage: %s\n", $conn->error);
+}
+?>
