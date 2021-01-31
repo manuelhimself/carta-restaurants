6c2e97bcb70ee32b14558acc706f37246a2d7fd8
JaumeLlullRossello <60427726+JaumeLlullRossello@users.noreply.github.com>
2021-01-31 21:56:09 +0100

    començament donar estils editarCarta

--- back/js/editarCarta.js
+++ back/js/editarCarta.js
@@ -4,15 +4,14 @@ $(document).ready(function () {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                $("#cards").html(this.responseText);
+                $("#cartes").html(this.responseText);
                 var cartes = JSON.parse(this.responseText);
-                console.log(cartes);
                 var i = 0;
                 for (c in cartes) {
 
-                    if (i % 2 == 0) {
+                    if (i % 3 == 0) {
                         var rowDIV = $("<div/>", { class: "row" });
-                        $("#cards").append(rowDIV);
+                        $("#cartes").append(rowDIV);
                     }
                     i++;
 
@@ -21,19 +20,19 @@ $(document).ready(function () {
                     var actiu = cartes[c].actiu;
 
                     var colDIV = $("<div/>", { class: "col-md-4" });
-                    var cardIMG = $("<img/>", { class: "card-img-top", src: "", alt: "card image", style: "width:100%" });
                     var cardDIV = $("<div/>", { class: "card", id: idCarta });
                     var cardBody = $("<div/>", { class: "card-body" });
+                    var cardIcon =  $("<i/>",{ class: "fa fa-trash"});
                     var cardH4 = $("<h4/>", { class: "card-title", text: nom });
-                    var cardP = $("<p/>", { class: "card-text", text: nom });
-                    var cardA1 = $("<button/>", { type: "button", class: "editarCarta btn btn-primary", id: "edC" + idCarta, text: "Mostra seccions" });
-                    var cardA2 = $("<button/>", { type: "button", class: "eliminarCarta btn btn-secondary", id: "elC" + idCarta, text: "Eliminar" });
-                    cardDIV.append(cardIMG);
-                    cardBody.append(cardH4, cardP, cardA1, cardA2);
+                    var cardA1 = $("<button/>", { type: "button", class: "editarCarta btn", id: "edC" + idCarta, text: "Mostra seccions" });
+                    var cardA2 = $("<a/>", {class: "eliminarCarta btn", id: "elC" + idCarta});
+                    cardA2.append(cardIcon);
+                    cardBody.append(cardH4, cardA1, cardA2);
                     cardDIV.append(cardBody);
                     colDIV.append(cardDIV);
                     rowDIV.append(colDIV);
                 }
+                aplicarEstilCartes();
             }
         };
         xhttp.open("POST", "../../api/carta/read.php", true);
@@ -44,20 +43,19 @@ $(document).ready(function () {
 
     function mostrarSeccions(idC) {
         var xhttp = new XMLHttpRequest();
-        console.log("mostrar seccions fora");
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                console.log("mostrar seccions dins");
                 $("#seccions").html(this.responseText);
                 var seccions = JSON.parse(this.responseText);
-                var botoAfegirSeccio = $("<button/>", { type: "button", class: "btn btn-secondary", id: "afegirSeccio", idCarta: idC, text: "Afegir secció" });
-                $("#seccions").append(botoAfegirSeccio);
+                var botoAfegirSeccio = $("<button/>", { type: "button", class: "btn", id: "afegirSeccio", idCarta: idC, text: "Afegir secció" });
+                var titol = $("<h1/>",{text: "Seccions"});
+                $("#seccions").append(titol,botoAfegirSeccio);
                 var i = 0;
                 for (s in seccions) {
 
-                    if (i % 2 == 0) {
+                    if (i % 1 == 0) {
                         var rowDIV = $("<div/>", {
-                            class: "row",
+                            class: "row justify-content-md-center",
                         });
                         $("#seccions").append(rowDIV);
                     }
@@ -67,23 +65,61 @@ $(document).ready(function () {
                     var idSeccio = seccions[s].idSeccio;
                     var nom = seccions[s].nom;
 
-                    var colDIV = $("<div/>", { class: "col-md-4" });
+                    var colDIV = $("<div/>", { class: "col-md-10" });
                     var cardDIV = $("<div/>", { class: "card" });
                     var cardBody = $("<div/>", { class: "card-body" });
                     var cardH4 = $("<h4/>", { class: "card-title", text: nom });
-                    var cardP = $("<p/>", { class: "card-text", text: idCarta + " " + idSeccio });
-                    var cardA1 = $("<button/>", { type: "button", class: "editarSeccioNom btn btn-primary", id: "edS" + idSeccio, idCarta: idCarta, text: "Editar Nom" });
-                    var cardA2 = $("<button/>", { type: "button", class: "editarPlats btn btn-secondary", id: "edP" + idSeccio, text: "Editar Plats" });
-                    var cardA3 = $("<button/>", { type: "button", class: "eliminarSeccio btn btn-info", id: "elS" + idSeccio, idCarta: idCarta, text: "Eliminar" });
-                    cardBody.append(cardH4, cardP, cardA1, cardA2, cardA3);
+                    var cardIcon1 =  $("<i/>",{ class: "fa fa-trash"});
+                    var cardA1 = $("<button/>", { type: "button", class: "editarSeccioNom btn", id: "edS" + idSeccio, idCarta: idCarta, text: "Editar Nom" });
+                    var cardA2 = $("<button/>", { type: "button", class: "editarPlats btn", id: "edP" + idSeccio, text: "Editar Plats" });
+                    var cardA3 = $("<a/>", { class: "eliminarSeccio btn", id: "elS" + idSeccio, idCarta: idCarta});
+                    var cardIcon2 =  $("<i/>",{ class: "fa fa-edit"});
+                    var cardIcon3 =  $("<i/>",{ class: "fa fa-edit"});
+                    var groupButton = $("<div/>",{ class:"btn-group"});
+                    var list = $("<ul/>", {id: "llistaBotons"});
+                    var li1 = $("<li/>");
+                    var li2 = $("<li/>");
+                    var li3 = $("<li/>");
+                    cardA1.append(cardIcon3);
+                    cardA2.append(cardIcon2);
+                    cardA3.append(cardIcon1);
+                    li1.append(cardA1);
+                    li2.append(cardA2);
+                    li3.append(cardA3);
+                    list.append(li1, li2, li3);
+                    groupButton.append(list);
+                    cardBody.append(cardH4, groupButton);
                     cardDIV.append(cardBody);
                     colDIV.append(cardDIV);
                     rowDIV.append(colDIV);
                 }
+                aplicarEstilSeccions();
             }
         };
         xhttp.open("POST", "../../api/seccio/readByIdCarta.php?idCarta=" + idC, true);
         xhttp.send();
+    }
+
+    function aplicarEstilCartes(){
+        $(".card").css("background-color","#414749");
+        $(".card").css("color","#FFFFFF");
+        $(".btn").css("background-color"," #A33E3E");
+        $(".btn").css("border-color"," #FFFFFF");
+        $(".btn").css("color"," #FFFFFF");
+        $("a").css("color","#FFFFFF");
+        $(".card").css("height","100%");
+    }
+
+    function aplicarEstilSeccions(){
+        $(".card").css("background-color","#414749");
+        $(".card").css("color","#FFFFFF");
+        $(".btn").css("background-color"," #A33E3E");
+        $(".btn").css("border-color"," #FFFFFF");
+        $(".btn").css("color"," #FFFFFF");
+        $(".btn-group").css("float", "right");
+        $("h4").css("float", "left");
+        $("ul").css("list-style-type", "none");
+        $("li").css("margin-top","5%");
     }
 
     function eliminarSeccio(idS, idC) {
@@ -131,11 +167,9 @@ $(document).ready(function () {
     }
 
     function afegirSeccio(nomSeccio,idC){
-        console.log("afegirSeccio fora");
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                console.log("afegirSeccio dins");
                 mostrarSeccions(idC);
             }
         };
@@ -173,6 +207,7 @@ $(document).ready(function () {
         var idBoto = $(this).attr("id");
         var idCarta = idBoto.substr(3, idBoto.length);
         eliminarCarta(idCarta);
+        $("#seccions").empty();
     });
 
     $(document).on("click", "#afegirCarta", function () {
@@ -180,10 +215,9 @@ $(document).ready(function () {
     });
 
     $(document).on("click", "#bAfegirCarta", function () {
-        console.log("dins");
         var nomCarta = $("#nomCartaForm").val();
-        console.log(nomCarta);
         afegirCarta(nomCarta);
+        $("#seccions").empty();
     });
 
     $(document).on("click", "#afegirSeccio", function () {
