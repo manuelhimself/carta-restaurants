5a2314205fde4b0973bd6ad84c7f64308b40fe82
manuelhimself <manuel.pe97a.aema@gmail.com>
2021-02-03 12:05:35 +0100

    fixed headers?

--- api/establiment/update.php
+++ api/establiment/update.php
@@ -2,7 +2,8 @@
 
 // Headers
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: http://api.restaurat.me'); 
+header('Access-Control-Allow-Credentials', 'true');
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization");
 
--- api/seccio/read.php
+++ api/seccio/read.php
@@ -1,6 +1,7 @@
 <?php 
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://api.restaurat.me'); 
+header('Access-Control-Allow-Credentials', 'true');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET');
 
