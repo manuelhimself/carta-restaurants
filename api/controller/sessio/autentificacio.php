1c3d2dca7db78626d51cdd4327b50fc148e0e36f
manuelhimself <manuel.pe97a.aema@gmail.com>
2021-02-11 09:41:02 +0100

    Update autentificacio.php

--- api/controller/sessio/autentificacio.php
+++ api/controller/sessio/autentificacio.php
@@ -3,29 +3,27 @@
 $conn = new mysqli("localhost", "root", "password", "restaurat");
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
+	die("Connection failed: " . $conn->connect_error);
+}
 
 $email = $_REQUEST["email"];
 $psswd = $_REQUEST["password"];
 
-$sql = "SELECT nom, password FROM establiment WHERE correu_electronic = '" . $email . "' and password='".$psswd."';";
+$sql = "SELECT nom, password FROM establiment WHERE correu_electronic = '" . $email . "' and password='" . $psswd . "';";
 
-$query=mysqli_query($conn,$sql);
-$counter=mysqli_num_rows($query);
-if ($counter==1){
-		$sqly = "SELECT id FROM establiment WHERE correu_electronic = '" . $email . "' and password='".$psswd."';";
-		$resultat = $conn->query($sqly);
-    		if ($resultat){
-        		while($row = $resultat->fetch_array()){
-				$id = $row['id'];
-				}
-			}
-		session_start();
-		echo json_encode($id);
+$query = mysqli_query($conn, $sql);
+$counter = mysqli_num_rows($query);
+if ($counter == 1) {
+	$sqly = "SELECT id FROM establiment WHERE correu_electronic = '" . $email . "' and password='" . $psswd . "';";
+	$resultat = $conn->query($sqly);
+	if ($resultat) {
+		while ($row = $resultat->fetch_array()) {
+			$id = $row['id'];
+		}
+	}
+	session_start();
+	$_SESSION['establiment'] = $id;
+	echo "OK";
 } else {
-		echo "ERROR";
-		
+	echo "ERROR";
 }
-
-?>
