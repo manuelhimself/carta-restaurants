23dd42de12615081aaf568531308959cc15e77fb
manuelhimself <manuel.pe97a.aema@gmail.com>
2021-02-11 09:49:47 +0100

    Update reserves.js

--- back/js/reserves.js
+++ back/js/reserves.js
@@ -1,4 +1,4 @@
-//var reserves;
+var reserves;
 var grafic;
 var taula;
 
@@ -7,32 +7,32 @@ function loadData() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var reserves = JSON.parse(this.responseText);
-            var taula = $('#reserva').DataTable({
+            reserves = JSON.parse(this.responseText);
+            taula = $("#reserva").DataTable({
                 data: reserves,
-                dom: 'Bfrtip',
+                dom: "Bfrtip",
                 resposive: true,
-                buttons: [
-                    'copy', 'excel', 'pdf'
-                ],
+                buttons: ["copy", "excel", "pdf"],
                 columns: [{
-                    "data": 'nomUsuari'
-                }, {
-                    "data": 'hora'
-                }, {
-                    "data": 'comensals'
-                }, ],
+                        data: "nomUsuari",
+                    },
+                    {
+                        data: "hora",
+                    },
+                    {
+                        data: "comensals",
+                    },
+                ],
                 responsive: true,
                 order: [
-                    [1, 'asc']
+                    [1, "asc"]
                 ],
                 language: {
-                    url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/Catalan.json"
+                    url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/Catalan.json",
                 },
                 select: true,
-                destroy: true
+                destroy: true,
             });
-
         }
     };
     xhttp.open("POST", api + "/reserves/reserves.php?data=" + d, false);
@@ -43,44 +43,46 @@ var margin = {
         top: 30,
         right: 30,
         bottom: 70,
-        left: 60
+        left: 60,
     },
     width = 460 - margin.left - margin.right,
     height = 460 - margin.top - margin.bottom;
 
-var svg = d3.select("#svg")
+var svg = d3
+    .select("#svg")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.json(api + "/reserves/graficReserves.php", function(data) {
     data.sort(function(b, a) {
         return a.Value - b.Value;
     });
 
-    var x = d3.scaleBand()
+    var x = d3
+        .scaleBand()
         .range([0, width])
-        .domain(data.map(function(d) {
-            return d.mes;
-        }))
+        .domain(
+            data.map(function(d) {
+                return d.mes;
+            })
+        )
         .padding(0.2);
-    svg.append("g")
+    svg
+        .append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
         .selectAll("text")
         .attr("transform", "translate(-10,0)rotate(-45)")
         .style("text-anchor", "end");
 
-    var y = d3.scaleLinear()
-        .domain([0, 100])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
+    var y = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+    svg.append("g").call(d3.axisLeft(y));
 
-    svg.selectAll("mybar")
+    svg
+        .selectAll("mybar")
         .data(data)
         .enter()
         .append("rect")
@@ -94,5 +96,5 @@ d3.json(api + "/reserves/graficReserves.php", function(data) {
         .attr("height", function(d) {
             return height - y(d.suma);
         })
-        .attr("fill", "#A33E3E")
-})
+        .attr("fill", "#A33E3E");
+});
