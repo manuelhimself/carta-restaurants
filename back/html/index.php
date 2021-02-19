c89e937895fe5c1aa7746a7cf711de786c24a1c7
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-02-19 18:16:00 +0100

    rutes scripts

--- back/html/index.php
+++ back/html/index.php
@@ -5,21 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../css/dist/style.css">
+    <link rel="stylesheet" href="css/dist/style.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="icon" href="html/images/icon/icon.png">
-    <script type="text/javascript" src="html/js/api.js"></script>
-    <script type="text/javascript" src="html/js/editaPerfil.js"></script>
-    <script src="./js/closeSesion.js"></script>
+    <link rel="icon" href="images/icon/icon.png">
+    <script type="text/javascript" src="js/api.js"></script>
+    <script type="text/javascript" src="js/editaPerfil.js"></script>
+    <script src="js/closeSesion.js"></script>
     <title>Edita perfil</title>
 </head>
 
 <body>
 <?
-include("navbar.php");
+include("navBar.php");
 ?>
     <div class="container" id="perfilContainer">
         <div class="card" id="perfilCard">
