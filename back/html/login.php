5bb15168dfde5c67f66b6d273a162971555df0d1
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-02-19 18:39:07 +0100

    prova login

--- back/html/login.php
+++ back/html/login.php
@@ -1,4 +1,6 @@
-
+<?
+include 'navBar.php';
+?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -10,7 +12,6 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script type="text/javascript" src="js/api.js"></script>
     <script type="text/javascript" src="js/login.js"></script>
     <link rel="stylesheet" href="css/dist/style.css">
 </head>
