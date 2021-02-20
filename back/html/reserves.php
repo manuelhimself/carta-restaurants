70abb1cf3fbfc4597385958a69ceb8c85ee4dc64
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-02-20 10:41:42 +0100

    conexio a sa api ediarCarta editarPlat

--- back/html/reserves.php
+++ back/html/reserves.php
@@ -26,12 +26,12 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/dist/style.css">
+    <link rel="stylesheet" href="/css/dist/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <link rel="icon" href="images/icon/icon.png">
-    <script type="text/javascript" src="js/reserves.js"></script>
-    <script src="js/closeSesion.js"></script>
+    <link rel="icon" href="/images/icon/icon.png">
+    <script type="text/javascript" src="/js/reserves.js"></script>
+    <script src="/js/closeSesion.js"></script>
 
 
 </head>
