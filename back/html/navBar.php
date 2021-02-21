cb8d124380d9f7da1ab7a124b34b47f39af760e1
JaumeLlullRossello <60427726+JaumeLlullRossello@users.noreply.github.com>
2021-02-21 16:51:07 +0100

    Corregit navbar link icon

--- back/html/navBar.php
+++ back/html/navBar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark bg-dark sticky-top">
     <!--Logo-->
-    <a class="navbar-brand" href="index.html">
+    <a class="navbar-brand" href="index.php">
         <img id="logo" src="/images/icon/icon.png" alt="Logo">
         <script src="js/closeSesion.js"></script>
     </a>
