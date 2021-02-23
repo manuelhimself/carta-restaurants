e82dad4bab5587508d0bdbdaa86863c6eeb71ab1
JaumeLlullRossello <60427726+JaumeLlullRossello@users.noreply.github.com>
2021-02-23 12:19:13 +0100

    modificat navbar

--- back/html/navbarLoginRegister.php
+++ back/html/navbarLoginRegister.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-dark navbar-dark bg-dark sticky-top">
+<nav class="navbar navbar-expand-lg sticky-top">
     <!--Logo-->
     <a class="navbar-brand" href="./index.php">
         <img id="logo" src="/images/icon/icon.png" alt="Logo">
