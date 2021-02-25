eb45a5cfbc71a86fd0dbf05cf14c3131e5de868f
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-02-25 19:40:51 +0100

    build react

--- front/src/Components/fontawesome.js
+++
@@ -1,6 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {faPhoneAlt} from '@fortawesome/free-solid-svg-icons';
-import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
-library.add(faPhoneAlt,
-    faEnvelope);
-
