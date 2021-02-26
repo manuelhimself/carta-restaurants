e3cb1672da417d08b471250b05b051ac312f4fed
JaumeLlullRossello <60427726+JaumeLlullRossello@users.noreply.github.com>
2021-02-26 22:43:30 +0100

    modificat codi navbar-collapse de navbarlogin color icone

--- back/html/navbarLoginRegister.php
+++ back/html/navbarLoginRegister.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg sticky-top">
+<nav class="navbar navbar-expand-lg sticky-top navbar-light">
     <!--Logo-->
     <a class="navbar-brand" href="./index.php">
         <img id="logo" src="/images/icon/icone.png" alt="Logo">
