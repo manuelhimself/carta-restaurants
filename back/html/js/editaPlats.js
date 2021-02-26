972d67a59fc47ed537eb397b62c1ce9036f6ec2d
JaumeLlullRossello <60427726+JaumeLlullRossello@users.noreply.github.com>
2021-02-26 18:30:07 +0100

    modificat modal editarPlat

--- back/html/js/editaPlats.js
+++ back/html/js/editaPlats.js
@@ -159,7 +159,9 @@ $(document).ready(function() {
 
     function afegirCheckbox() {
         $(".checkbox").empty();
+        var p = $("<p/>", {text: "Alergens: "});
         var form = $(".checkbox");
+        form.append(p);
         for (var i = 0; i < alergens.length; i++) {
             var id = alergens[i].idAlergen;
             var nom = alergens[i].nom;
