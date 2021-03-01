a4ca8f269d9d73ba39bb1902da726da83dfb80fa
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-03-01 09:48:03 +0100

    multiidioma categoria

---
+++ api/controller/categoria/categoriaIdioma.php
@@ -0,0 +1,19 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+$idioma = $_REQUEST['idioma'];
+
+$conn = new mysqli("localhost","root","password","restaurat"); 
+
+$select = $conn -> prepare("SELECT c.id, tc.nom
+FROM categoria c
+inner join traduccio_categoria tc on tc.id_categoria = c.id
+inner join llenguatge l on l.id = tc.id_llenguatge and l.nom = $idioma");
+$select -> execute();
+$resultat = $select->get_result();
+$mostrar = $resultat->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($mostrar);
+?>
