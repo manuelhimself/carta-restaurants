f73d7a135662450b1e5b9fbc51c0640aafdb3c84
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-03-01 11:07:51 +0100

    categoria.php

--- api/controller/categoria/categoriaIdioma.php
+++ api/controller/categoria/categoriaIdioma.php
@@ -7,7 +7,7 @@ $idioma = $_REQUEST['idioma'];
 
 $conn = new mysqli("localhost","root","password","restaurat"); 
 
-$select = $conn -> prepare("SELECT c.id, tc.nom
+$select = $conn -> connect() -> prepare("SELECT c.id, tc.nom
 FROM categoria c
 inner join traduccio_categoria tc on tc.id_categoria = c.id
 inner join llenguatge l on l.id = tc.id_llenguatge and l.nom = '$idioma' ");
