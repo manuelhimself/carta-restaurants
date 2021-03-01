5210006b23e4955cdf1cb01d7e77c317985faa5c
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-03-01 13:10:15 +0100

    categoria idioma

--- api/controller/categoria/categoriaIdioma.php
+++ api/controller/categoria/categoriaIdioma.php
@@ -4,12 +4,16 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
+if(isset($_REQUEST['idioma'])){
+    $idioma = $_REQUEST['idioma']; 
+ }
+
 
 $conn = new mysqli("localhost","root","password","restaurat");
 $select = $conn -> prepare("SELECT c.id, tc.nom
 FROM categoria c
 inner join traduccio_categoria tc on tc.id_categoria = c.id
-inner join llenguatge l on l.id = tc.id_llenguatge and l.nom = 'en'  ");
+inner join llenguatge l on l.id = tc.id_llenguatge and l.nom =$idioma");
 $select -> execute();
 $resultat = $select->get_result();
 $mostrar = $resultat->fetch_all(MYSQLI_ASSOC);
