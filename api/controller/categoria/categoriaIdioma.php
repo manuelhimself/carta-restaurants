7377c8b632cef723795977460697cb4787b190b2
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-03-01 13:04:14 +0100

    categoriaIdoma.php

--- api/controller/categoria/categoriaIdioma.php
+++ api/controller/categoria/categoriaIdioma.php
@@ -1,28 +1,18 @@
 <?php
+
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
-$idioma = $_REQUEST['idioma'];
 
-$conn = new mysqli("localhost","root","password","restaurat"); 
-
-$select = $conn -> connect() -> prepare("SELECT c.id, tc.nom
+$conn = new mysqli("localhost","root","password","restaurat");
+$select = $conn -> prepare("SELECT c.id, tc.nom
 FROM categoria c
 inner join traduccio_categoria tc on tc.id_categoria = c.id
-inner join llenguatge l on l.id = tc.id_llenguatge and l.nom = ".$idioma." ");
+inner join llenguatge l on l.id = tc.id_llenguatge and l.nom = 'en'  ");
 $select -> execute();
 $resultat = $select->get_result();
+$mostrar = $resultat->fetch_all(MYSQLI_ASSOC);
 
-$traduccions = array();
-
-while ($row = $result->fetch_assoc()) {
-    $traduccionActual = array(
-        'id' => $row['id'],
-        'nom' => $row['nom'],
-    );
-    array_push($traduccions, $traduccionActual);
-}
-
-echo json_encode($traduccions);
+echo json_encode($mostrar);
 ?>
