69952975be98409856395739733385d1369380c2
manuelhimself <manuel.pe97a.aema@gmail.com>
2021-03-02 12:49:30 +0100

    Update email.php

--- back/html/email.php
+++ back/html/email.php
@@ -1,5 +1,7 @@
 <?php
 
-if(mail('manuel.pe97a.aema@gmail.com', 'Prova php', 'Això és una prova de mail en php')){
+if (mail('manuel.pe97a.aema@gmail.com', 'Prova php', 'Això és una prova de mail en php')) {
     echo "Email sent";
+} else {
+    echo 'Email not sent';
 }
