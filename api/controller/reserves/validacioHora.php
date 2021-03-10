58bc7afc287886dd0fb2f684f69289f412df4a52
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-03-10 13:05:33 +0100

    cors reserva

--- api/controller/reserves/validacioHora.php
+++ api/controller/reserves/validacioHora.php
@@ -1,6 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *'); 
 header('Access-Control-Allow-Credentials', 'true');
+header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: DELETE');
 
 $conn = new mysqli("localhost", "root", "password", "restaurat");
